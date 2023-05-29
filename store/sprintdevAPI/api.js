bc4befe4f68be65e96156919710b9c3a29342a5b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-05-29 16:20:04 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/sprintdevAPI/api.js
+++ store/sprintdevAPI/api.js
@@ -1,55 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const sprintdevAPI = axios.create({
   baseURL: "https://sprint41-dev-85625.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return sprintdevAPI.post(`/api/v1/login/`, payload.data)
+  return sprintdevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return sprintdevAPI.post(`/api/v1/signup/`, payload.data)
+  return sprintdevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return sprintdevAPI.get(`/rest-auth/user/`)
+  return sprintdevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return sprintdevAPI.put(`/rest-auth/user/`, payload.data)
+  return sprintdevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return sprintdevAPI.patch(`/rest-auth/user/`, payload.data)
+  return sprintdevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return sprintdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/login/`, payload.data)
+  return sprintdevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return sprintdevAPI.get(`/rest-auth/logout/`)
+  return sprintdevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/logout/`)
+  return sprintdevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/registration/`, payload.data)
+  return sprintdevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return sprintdevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return sprintdevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return sprintdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return sprintdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return sprintdevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return sprintdevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -65,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
